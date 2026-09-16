fd3d48009d8df435fa7aee84e7c51ddbbc5a7fc6
Igor Gałuszka <igor.galuszka01@gmail.com>
2026-09-16 10:17:07 +0000

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import json
+from pathlib import Path
 
-options = input("===Wybierz jedną z opcji=== \n1.Sprzedający \n2.Kupujący \n3.Zakończ")
-if options == "1":
-    seller()
-elif options == "2":
-    print()
-elif options == "3":
-    print()
-else:
-    print("Nieprawidłowy wybór")    
+DB_Path = Path("books_db.json")
+
