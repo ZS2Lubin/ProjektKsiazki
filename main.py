248a4cbb20f8e86c45599202d2d70ba7975353ef
Igor Gałuszka <igor.galuszka01@gmail.com>
2026-09-16 10:12:12 +0000

    k

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+options = input("===Wybierz jedną z opcji=== \n1.Sprzedający \n2.Kupujący \n3.Zakończ")
+if options == "1":
+    seller()
+elif options == "2":
+    print()
+elif options == "3":
+    print()
+else:
+    print("Nieprawidłowy wybór")    
